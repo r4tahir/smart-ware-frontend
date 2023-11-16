3db6862d9c039c427200a72aac5c25a66db4c8a5
unknown <rtahir0786@gmail.com>
2023-11-16 18:38:47 -0500

    Updates

---
+++ precache-manifest.306017ac7aa01be16e93de42d95f081a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d74f01b279c3167a623ea3b98eb105e",
+    "url": "/smart-ware-frontend/index.html"
+  },
+  {
+    "revision": "88abf8c2c9ba0f4d7e91",
+    "url": "/smart-ware-frontend/static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "88abf8c2c9ba0f4d7e91",
+    "url": "/smart-ware-frontend/static/js/2.fa3444bf.chunk.js"
+  },
+  {
+    "revision": "1f152a4ac5c8a24a9d1f415aafdcdff4",
+    "url": "/smart-ware-frontend/static/js/2.fa3444bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a08828b237074bffc3",
+    "url": "/smart-ware-frontend/static/js/main.18f2cf2b.chunk.js"
+  },
+  {
+    "revision": "4b0455d4031923472e43",
+    "url": "/smart-ware-frontend/static/js/runtime-main.5c1e7517.js"
+  }
+]);
